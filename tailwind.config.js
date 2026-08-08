dbdf3c29442a638cbdb473971af73ade53f4f24a
tuanhoangfx <tuanhoangfx@gmail.com>
2026-08-08 16:11:00 +0700

    P0008 v0.1.5: release

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./vendor/hub-ui/src/**/*.{js,ts,jsx,tsx}",
+    "./vendor/hub-identity/src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
